dbb128d6f98c0cec472a15ef1bf5d5da4713858f
radostinslavcev1-lab <radostinslavcev1@gmail.com>
2026-03-12 16:31:05 +0200

    Update server.py

--- server.py
+++ server.py
@@ -95,7 +95,8 @@ if __name__ == '__main__':
     print("🚀 СЪРВЪРЪТ РАБОТИ (С Умна Опашка!)")
     print("👉 ОТВОРИ БРАУЗЪРА СИ И ВЛЕЗ НА АДРЕС: http://localhost:5000")
     print("="*60 + "\n")
-    if __name__ == '__main__':
+ if __name__ == '__main__':
+    import os
     port = int(os.environ.get("PORT", 10000))
     print(f"\n🚀 Сървърът работи онлайн! Порт: {port}")
     app.run(host="0.0.0.0", port=port)
